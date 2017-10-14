3de57b25b27a75649a3de791352b953cb8964044
gregpaton08 <gregpaton08@gmail.com>
2017-10-14 16:39:05 -0400

    Fix logic

--- Home Lock/SettingsTableViewController.swift
+++ Home Lock/SettingsTableViewController.swift
@@ -16,22 +16,14 @@ class SettingsTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
-        if let address = HLSettings.getSetting(.address) as? String, let port = HLSettings.getSetting(.port) as? String {
+        if let address = HLSettings.getSetting(.address) as? String {
             addressTextField.text = address
+        }
+        
+        if let port = HLSettings.getSetting(.port) as? String {
             portTextField.text = port
         }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func viewWillDisappear(_ animated: Bool) {
