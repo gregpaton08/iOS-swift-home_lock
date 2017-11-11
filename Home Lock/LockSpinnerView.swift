b4aa09a4f0627b11561a39f7196b8f2b9c89a0e6
gregpaton08 <gregpaton08@gmail.com>
2017-11-11 18:46:26 -0500

    Add file for a lock spinner view

---
+++ Home Lock/LockSpinnerView.swift
@@ -0,0 +1,21 @@
+//
+//  LockSpinnerView.swift
+//  Home Lock
+//
+//  Created by Greg Paton on 11/11/17.
+//  Copyright © 2017 GSP. All rights reserved.
+//
+
+import UIKit
+
+class LockSpinnerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
