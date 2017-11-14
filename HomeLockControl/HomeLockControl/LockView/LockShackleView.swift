d74e070b914773c03bf18b029e08cee4f3a30db2
gregpaton08 <gregpaton08@gmail.com>
2017-11-13 21:55:04 -0500

    Copy LockView files into framework for use in widget

---
+++ HomeLockControl/HomeLockControl/LockView/LockShackleView.swift
@@ -0,0 +1,52 @@
+//
+//  LockShackleView.swift
+//  HomeLockControl
+//
+//  Created by Greg Paton on 11/13/17.
+//  Copyright © 2017 GSP. All rights reserved.
+//
+
+import UIKit
+
+class LockShackleView: UIView {
+    
+    public var isLocked = true
+    public var lockColor = UIColor.black
+    
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath()
+        lockColor.setFill()
+        path.lineWidth = 1.0
+        
+        let center = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
+        var lockViewSize = self.bounds.size
+        
+        // The lock dimensions are conceptualized as "units" so that the lock can be generically drawn given any view size. For reference, the total size required by the lock view is 16 units wide and 10 units high. The lock body is 8 units wide and 6 units tall. Since the lock view size is 10 units in height, we can convert from units to actual points by multiply the height by the number of units and dividing by 10.
+        func pointsFrom(units: CGFloat) -> CGFloat {
+            return lockViewSize.height * units / 10.0
+        }
+        
+        // Draw the lock shackle.
+        var lockShackleCenter: CGPoint
+        if isLocked {
+            lockShackleCenter = CGPoint(x: center.x, y: center.y - pointsFrom(units: 2.5))
+        } else {
+            lockShackleCenter = CGPoint(x: center.x + pointsFrom(units: 5), y: center.y - pointsFrom(units: 2.5))
+        }
+        let lockShackleOuterEdgeRadius = pointsFrom(units: 2.5)
+        let lockShackleOuterEdgeStart = CGPoint(x: lockShackleCenter.x - lockShackleOuterEdgeRadius, y: lockShackleCenter.y + pointsFrom(units: 1.5))
+        let lockShackleOuterEdgeEnd = CGPoint(x: lockShackleCenter.x + lockShackleOuterEdgeRadius, y: lockShackleCenter.y + pointsFrom(units: 1.5))
+        let lockShackleInnerEdgeRadius = pointsFrom(units: 1.5)
+        let lockShackleInnerEdgeStart = CGPoint(x: lockShackleCenter.x - lockShackleInnerEdgeRadius, y: lockShackleCenter.y + pointsFrom(units: 1.5))
+        let lockShackleInnerEdgeEnd = CGPoint(x: lockShackleCenter.x + lockShackleInnerEdgeRadius, y: lockShackleCenter.y + pointsFrom(units: 1.5))
+        path.move(to: lockShackleOuterEdgeStart)
+        path.addArc(withCenter: lockShackleCenter, radius: lockShackleOuterEdgeRadius, startAngle: CGFloat.pi, endAngle: 0, clockwise: true)
+        path.addLine(to: lockShackleOuterEdgeEnd)
+        path.addLine(to: lockShackleInnerEdgeEnd)
+        path.addArc(withCenter: lockShackleCenter, radius: lockShackleInnerEdgeRadius, startAngle: 0, endAngle: CGFloat.pi, clockwise: false)
+        path.addLine(to: lockShackleInnerEdgeStart)
+        path.close()
+        path.fill()
+    }
+}
+
---
+++ HomeLockControl/HomeLockControl/LockView/LockSpinnerView.swift
@@ -0,0 +1,26 @@
+//
+//  LockSpinnerView.swift
+//  HomeLockControl
+//
+//  Created by Greg Paton on 11/13/17.
+//  Copyright © 2017 GSP. All rights reserved.
+//
+
+import UIKit
+
+class LockSpinnerView: UIView {
+    
+    var spinnerColor = UIColor.white
+    
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath()
+        path.lineWidth = 2.0
+        spinnerColor.setStroke()
+        
+        let center = CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
+        let radius = (min(self.bounds.width, self.bounds.height) / 2) - 1
+        path.addArc(withCenter: center, radius: radius, startAngle: -CGFloat.pi / 2, endAngle: -CGFloat.pi / 3, clockwise: false)
+        path.stroke()
+    }
+    
+}
