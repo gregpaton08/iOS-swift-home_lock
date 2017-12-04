1b661ed5801d641294dd235ac6579d94bf2629c8
gregpaton08 <gregpaton08@gmail.com>
2017-12-04 14:02:23 -0500

    Add file for lock body view

---
+++ HomeLockControl/HomeLockControl/LockView/LockBodyView.swift
@@ -0,0 +1,21 @@
+//
+//  LockBodyView.swift
+//  HomeLockControl
+//
+//  Created by Greg Paton on 12/4/17.
+//  Copyright © 2017 GSP. All rights reserved.
+//
+
+import UIKit
+
+class LockBodyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
