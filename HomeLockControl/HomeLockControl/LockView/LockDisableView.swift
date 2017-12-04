321a34bb3127f084ff327ccaa6ad9b3941329723
gregpaton08 <gregpaton08@gmail.com>
2017-12-04 12:15:49 -0500

    Add new file for lock view disabled

---
+++ HomeLockControl/HomeLockControl/LockView/LockDisableView.swift
@@ -0,0 +1,21 @@
+//
+//  LockDisableView.swift
+//  HomeLockControl
+//
+//  Created by Greg Paton on 12/4/17.
+//  Copyright © 2017 GSP. All rights reserved.
+//
+
+import UIKit
+
+class LockDisableView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
